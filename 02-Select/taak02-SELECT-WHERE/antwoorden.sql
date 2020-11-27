8815ee979f251b32280c6b98eb0e4e1d9044dbfc
Mike <mike.kouelambou@student.rocva.nl>
2020-11-27 12:08:48 +0100

    save changes

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -1,11 +1,11 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank=7
 -- Opdracht 2 
-
+SELECT rank FROM jaar2016 WHERE country="Netherlands"
 -- Opdracht 3
-
+SELECT score FROM jaar2015 WHERE country ="Netherlands"
 -- Opdracht 4
-
+SELECT score FROM jaar2016 WHERE country ="Netherlands"
 -- Opdracht 5
 
 -- Opdracht 6 
