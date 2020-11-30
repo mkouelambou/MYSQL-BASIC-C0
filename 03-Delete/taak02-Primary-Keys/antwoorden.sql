b0202deebecc23ede632353ea85d827a5264c98a
Mike <mike.kouelambou@student.rocva.nl>
2020-11-30 18:33:27 +0100

    save

--- 03-Delete/taak02-Primary-Keys/antwoorden.sql
+++ 03-Delete/taak02-Primary-Keys/antwoorden.sql
@@ -1,5 +1,8 @@
 -- Opdracht 3
-
+SELECT * FROM players WHERE id= 198329
 -- Opdracht 4
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 5
+DELETE FROM players WHERE id = 199304
+DELETE FROM players WHERE id = 207494
+DELETE FROM players WHERE id = 226753
